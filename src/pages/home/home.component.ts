318dcde18fcb91df7d413b9b83e6e94c40f1fc96
Syxfqn <msyaf0911@gmail.com>
2024-04-05 07:31:27 +0800

    Added click resume function

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  public openResume(): void {
+    window.open('https://drive.google.com/file/d/17d4fvZYEIObBb2RvuaOO1f5dDiz_UHla/view?usp=drive_link');
+  }
 }
