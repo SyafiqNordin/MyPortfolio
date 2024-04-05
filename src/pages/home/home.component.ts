c185c7c66a8e044b522234301f289ffd3de7ee8d
Syxfqn <msyaf0911@gmail.com>
2024-04-05 12:09:36 +0800

    Modified menu for phone

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  public menuButtonClicked = false;
+
   public openResume(): void {
     window.open('https://drive.google.com/file/d/17d4fvZYEIObBb2RvuaOO1f5dDiz_UHla/view?usp=drive_link');
+  }
+
+  public openMenu(): void {
+    this.menuButtonClicked = true;
+  }
+
+  public closeMenu(): void {
+    this.menuButtonClicked = false;
   }
 }
