79147bcd702b8507bc0e4380c7101f8615b10557
Syxfqn <msyaf0911@gmail.com>
2024-04-05 15:34:10 +0800

    Partially modified attribute cards section with mobile responsiveness

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   public menuButtonClicked = false;
+  public games: string[] = ['Valorant', 'Apex Legends', 'FIFA', 'UFC', 'GTA', 'Assassins Creed', 'Far Cry'];
+  public devTools: string[] = ['BitBucket', 'Github', 'Jira', 'Visual Studio Code', 'Azure DevOps', 'Karma', 'Jasmine'];
+  public countries: string[] = ['France', 'China', 'Vietnam', 'Singapore', 'Saudi', 'Thailand', 'Indonesia'];
 
   public openResume(): void {
     window.open('https://drive.google.com/file/d/17d4fvZYEIObBb2RvuaOO1f5dDiz_UHla/view?usp=drive_link');
