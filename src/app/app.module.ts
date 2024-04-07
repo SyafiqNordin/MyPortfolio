c3a17456d609e99b882513645f0ac3b38a409448
Syxfqn <msyaf0911@gmail.com>
2024-04-07 14:51:05 +0800

    remove unwanted module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ClipboardModule } from '@angular/cdk/clipboard';
-import { PopoverModule } from '@ngx-popovers/popover';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
